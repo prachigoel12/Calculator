6f620104315bb2c6a0a4755febc689abf95f0378
prachigoel12 <156950981+prachigoel12@users.noreply.github.com>
2024-11-24 21:57:23 +0530

    Add files via upload

---
+++ firstapp/src/main/java/com/app/firstapp/controller/MainController.java
@@ -0,0 +1,50 @@
+package com.app.firstapp.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import jakarta.servlet.http.HttpServletRequest;
+
+@Controller
+public class MainController {
+	
+	@GetMapping("/")
+	public String showIndex() {
+		return "index";
+	}
+	
+	 @PostMapping("/")
+	public String calc(HttpServletRequest request, RedirectAttributes attrib ) {  //servlet- java program which is execute in server
+		
+		 int n1=Integer.parseInt(request.getParameter("n1"));
+		 int n2=Integer.parseInt(request.getParameter("n1"));
+		 
+		 String op= request.getParameter("op");
+		 
+		 int res=0;
+		 if(op.equals("add")) 
+		 {
+			 res= n1+n2;
+		 }
+		 
+		 else if(op.equals("sub")) {
+			 res= n1-n2;
+		 }
+		 
+		 else if(op.equals("mult")) {
+			 res= n1*n2;
+		 }
+		 
+		 else if (op.equals("div")) {
+			 res= n1/n2;
+		 }
+		 
+//		 Sustem.out.println("Result= "+ res);
+		 
+		 String result= "Result= "+res;
+		 attrib.addFlashAttribute("result", result);
+		 return "redirect:/";  //to render index page
+	}
+}
